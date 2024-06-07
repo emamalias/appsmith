89e26d5895b900871196559a446260ab6cab8513
Vemparala Surya Vamsi <vamsi@appsmith.com>
2024-06-07 18:43:14 +0530

    evalTrigger mutation fix

--- app/client/src/workers/Evaluation/handlers/evalTrigger.ts
+++ app/client/src/workers/Evaluation/handlers/evalTrigger.ts
@@ -1,6 +1,14 @@
 import { dataTreeEvaluator } from "./evalTree";
-import type { EvalWorkerASyncRequest } from "../types";
+import type { EvalTreeResponseData, EvalWorkerASyncRequest } from "../types";
 import ExecutionMetaData from "../fns/utils/ExecutionMetaData";
+import { makeEntityConfigsAsObjProperties } from "@appsmith/workers/Evaluation/dataTreeUtils";
+import {
+  generateOptimisedUpdatesAndSetPrevState,
+  uniqueOrderUpdatePaths,
+} from "../helpers";
+import type { UpdateDataTreeMessageData } from "sagas/EvalWorkerActionSagas";
+import { MessageType, sendMessage } from "utils/MessageUtil";
+import { MAIN_THREAD_ACTION } from "@appsmith/workers/Evaluation/evalWorkerActions";
 
 export default async function (request: EvalWorkerASyncRequest) {
   const { data } = request;
@@ -27,14 +35,70 @@ export default async function (request: EvalWorkerASyncRequest) {
       { isAllAffected: true, ids: [] },
     );
 
-    dataTreeEvaluator.evalAndValidateSubTree(
+    const updateResponse = dataTreeEvaluator.evalAndValidateSubTree(
       evalOrder,
       unEvalTree.configTree,
       unEvalUpdates,
     );
-  }
+    const dataTree = makeEntityConfigsAsObjProperties(
+      dataTreeEvaluator.evalTree,
+      {
+        evalProps: dataTreeEvaluator.evalProps,
+      },
+    );
 
-  return dataTreeEvaluator.evaluateTriggers(
+    /** Make sure evalMetaUpdates is sanitized to prevent postMessage failure */
+    const evalMetaUpdates = JSON.parse(
+      JSON.stringify(updateResponse.evalMetaUpdates),
+    );
+
+    const staleMetaIds = updateResponse.staleMetaIds;
+    const unevalTree = dataTreeEvaluator.getOldUnevalTree();
+
+    const allUnevalUpdates = unEvalUpdates.map(
+      (update) => update.payload.propertyPath,
+    );
+    const completeEvalOrder = uniqueOrderUpdatePaths([
+      ...allUnevalUpdates,
+      ...evalOrder,
+    ]);
+
+    const updates = generateOptimisedUpdatesAndSetPrevState(
+      dataTree,
+      dataTreeEvaluator,
+      completeEvalOrder,
+    );
+    const evalTreeResponse: EvalTreeResponseData = {
+      updates,
+      dependencies: {},
+      errors: [],
+      evalMetaUpdates,
+      evaluationOrder: evalOrder,
+      jsUpdates: {},
+      logs: [],
+      unEvalUpdates,
+      isCreateFirstTree: false,
+      staleMetaIds,
+      removedPaths: [],
+      isNewWidgetAdded: false,
+      undefinedEvalValuesMap: dataTreeEvaluator?.undefinedEvalValuesMap || {},
+      jsVarsCreatedEvent: [],
+    };
+
+    const data: UpdateDataTreeMessageData = {
+      workerResponse: evalTreeResponse,
+      unevalTree,
+    };
+
+    sendMessage.call(self, {
+      messageType: MessageType.DEFAULT,
+      body: {
+        data,
+        method: MAIN_THREAD_ACTION.UPDATE_DATATREE,
+      },
+    });
+  }
+  const res = dataTreeEvaluator.evaluateTriggers(
     dynamicTrigger,
     dataTreeEvaluator.getEvalTree(),
     unEvalTree.configTree,
@@ -45,4 +109,6 @@ export default async function (request: EvalWorkerASyncRequest) {
       triggerMeta,
     },
   );
+
+  return res;
 }
